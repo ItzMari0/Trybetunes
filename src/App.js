d4b35197a50a27eae43e1d7b1b909e8aba15a7b1
ItzMari0 <msajr@outlook.com>
2022-08-01 19:45:23 -0300

    Requisito 2 Resolvido

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -13,14 +13,16 @@ class App extends React.Component {
     return (
       <BrowserRouter>
         <p>TrybeTunes</p>
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        {/* render={(props) => <Album {...props} />} */}
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route component={ NotFound } />
-        <Route exact path="/" component={ Login } />
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/search" component={ Search } />
+          <Route path="/album/:id" component={ Album } />
+          {/* render={(props) => <Album {...props} />} */}
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,72 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
+
+const LOGINCHARACTERS = 3;
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      isLoginButtonDisabled: true,
+      loading: true,
+      redirect: false,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ username: value });
+    if (value.length >= LOGINCHARACTERS) {
+      this.setState({ isLoginButtonDisabled: false });
+    } else {
+      this.setState({ isLoginButtonDisabled: true });
+    }
+  };
+
+  onButtonClick = async () => {
+    const { username } = this.state;
+    this.setState({ loading: false });
+    await createUser({ name: username });
+    this.setState({ loading: true });
+    this.setState({ redirect: true });
+  }
+
   render() {
+    const {
+      username,
+      isLoginButtonDisabled,
+      loading,
+      redirect,
+    } = this.state;
+
     return (
-      <div data-testid="page-login">Login</div>
-        <input type="text" data-testid="login-name-input" />
-        
+      <div data-testid="page-login">
+        <form>
+          <label htmlFor="login">
+            <input
+              type="text"
+              data-testid="login-name-input"
+              onChange={ this.onInputChange }
+              value={ username }
+              id="login"
+              placeholder="Digite seu usuário"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="login-submit-button"
+            disabled={ isLoginButtonDisabled }
+            onClick={ this.onButtonClick }
+          >
+            Entrar
+          </button>
+          {!loading && <Loading />}
+          {redirect && <Redirect to="/search" />}
+        </form>
+      </div>
     );
   }
 }
