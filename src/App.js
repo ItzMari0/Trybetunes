ea5029dbfad54df027b56748bc3f979cced52c71
ItzMari0 <msajr@outlook.com>
2022-08-01 23:10:56 -0300

    Refatorando Código

--- src/App.js
+++ src/App.js
@@ -11,19 +11,20 @@ import NotFound from './pages/NotFound';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <>
         <p>TrybeTunes</p>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          <Route path="/search" component={ Search } />
-          <Route path="/album/:id" component={ Album } />
-          {/* render={(props) => <Album {...props} />} */}
-          <Route path="/favorites" component={ Favorites } />
-          <Route path="/profile" component={ Profile } />
-          <Route path="/profile/edit" component={ ProfileEdit } />
-          <Route component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route exact path="/search" component={ Search } />
+            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/favorites" component={ Favorites } />
+            <Route exact path="/profile" component={ Profile } />
+            <Route exact path="/profile/edit" component={ ProfileEdit } />
+            <Route path="" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </>
     );
   }
 }
