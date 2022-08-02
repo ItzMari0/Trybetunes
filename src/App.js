ada1e192d09b6f261d0875ff6a11d595bc62a696
ItzMari0 <msajr@outlook.com>
2022-08-02 17:22:03 -0300

    Requisito 7 Resolvido

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/" component={ Login } />
             <Route exact path="/search" component={ Search } />
-            <Route exact path="/album/:id" component={ Album } />
+            <Route exact path="/album/:id" render={ (props) => <Album { ...props } /> } />
             <Route exact path="/favorites" component={ Favorites } />
             <Route exact path="/profile" component={ Profile } />
             <Route exact path="/profile/edit" component={ ProfileEdit } />
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,77 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
+import Loading from './Loading';
 
 class Album extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      musicList: [],
+    };
+  }
+
+  componentDidMount = () => {
+    this.musicFunction();
+  }
+
+  musicFunction = async () => {
+    const { match: { params: { id } } } = this.props;
+    const music = await getMusics(id);
+    this.setState({
+      musicList: music,
+      loading: false,
+    });
+  }
+
   render() {
+    const { musicList, loading } = this.state;
+    const filterList = musicList.filter((music) => music.kind === 'song');
     return (
       <div data-testid="page-album">
         <Header />
+        { loading ? (
+          <Loading />
+        ) : (
+          <div>
+            <div>
+              <img
+                src={ musicList[0].artworkUrl100 }
+                alt={ musicList[0].collectionName }
+              />
+              <p data-testid="artist-name">{ musicList[0].artistName }</p>
+              <p data-testid="album-name">{ musicList[0].collectionName }</p>
+            </div>
+            <div>
+              { filterList.map((music) => {
+                const { trackName, trackId, previewUrl } = music;
+                return (
+                  <MusicCard
+                    key={ trackId }
+                    trackName={ trackName }
+                    previewUrl={ previewUrl }
+                    trackId={ trackId }
+                  />
+                );
+              })}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
 
 export default Album;
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
